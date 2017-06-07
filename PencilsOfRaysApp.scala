2d1a98e9d1245d901b5ac0ea1a68d24c0cddbeb3
guangjunwu <guangjunwu@users.noreply.github.com>
2017-06-07 10:15:45 -0400

    Update PencilsOfRaysApp.scala

--- PencilsOfRaysApp.scala
+++ PencilsOfRaysApp.scala
@@ -1,9 +1,8 @@
 import scala.util.Random
 
-// https://projecteuler.net/problem=372
 object PencilsOfRaysApp extends App {
   
-  // show points, not efficient
+  // show points, not efficient, for debugging only
   def findLatticePoints(p: Long, min: Long, max: Long): Seq[(Long, Long)] = {
     // slope min and max
     val delta = 0
@@ -31,18 +30,20 @@ object PencilsOfRaysApp extends App {
     var x = x0
     var kx0 = k0*x0
     var kx1 = k1*x0
+    var y0 = 0L
+    var y1 = 0L
     while(x <= x1){
-      val y0 = Math.max(min, kx0.longValue())
-      val y1 = Math.min(max, kx1.longValue())
-      var c = y1 - y0 + 1
-      if( c > 0 && (y0 * y0 / x / x) != p ) c -= 1;
-      if( c > 0 && (y1 * y1 / x / x) != p ) c -= 1;
+      y0 = Math.max(min, kx0.longValue())
+      y1 = Math.min(max, kx1.longValue())
+      var c = y1 - y0 + 1L
+      if( c > 0L && (y0 * y0 / x / x) != p ) c -= 1L;
+      if( c > 0L && (y1 * y1 / x / x) != p ) c -= 1L;
       
-      if(c > 0) {
+      if(c > 0L) {
         count += c
       }
       
-      x += 1
+      x += 1L
       kx0 += k0
       kx1 += k1
       
@@ -55,8 +56,11 @@ object PencilsOfRaysApp extends App {
   def pencilsOfRays(m0: Long, n: Long): Unit = {
     val t0 = System.nanoTime()
     val m = m0 + 1L
-    val k = Math.floor(1.0 * n * n / m / m).longValue()
-    val total = Random.shuffle((1L to k by 2L).toList).par.map(countLatticePoints(_, m, n)).sum
+    val k = n * n / m / m
+    val processor = 4L
+    val oddNumbers = Random.shuffle((1L to k by 2L).toList)
+    
+    val total = oddNumbers.par.map(countLatticePoints(_, m, n)).sum
     print(s"==> There are ${total} lattice points in [$m0, $n]")
     val t1 = System.nanoTime()
     println("    Elapsed time: " + ((t1 - t0)/1000000) + " ms")
@@ -71,7 +75,7 @@ object PencilsOfRaysApp extends App {
 /* out put:
  *
  * ==> There are 3019 lattice points in [0, 100]    Elapsed time: 88 ms
- * ==> There are 29750422 lattice points in [100, 10000]    Elapsed time: 18 ms
- * ==> There are 301450079531438464 lattice points in [2000000, 1000000000]    Elapsed time: 3389769 ms
+ * ==> There are 29750422 lattice points in [100, 10000]    Elapsed time: 21 ms
+ * ==> There are 301450079531438464 lattice points in [2000000, 1000000000]    Elapsed time: 2877929 ms
  * 
  */
